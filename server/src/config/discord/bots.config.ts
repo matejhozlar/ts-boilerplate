d2c4d3edccc62e2b65f1919e24e185be093f0cb2
Matej <hozlarmatej0@gmail.com>
2025-12-04 11:58:52 +0100

    fix

--- server/src/config/discord/bots.config.ts
+++ server/src/config/discord/bots.config.ts
@@ -2,22 +2,40 @@ import { env } from "../env/env.config";
 
 export interface DiscordBotConfig {
   main: {
+    /**
+     * Discord application/bot ID used for identification
+     * Required for registering slash commands and API interactions
+     */
     id: string;
+    /**
+     * Discord bot authentication token
+     * Used to authenticate and login the bot to Discord's gateway
+     */
     token: string;
+    /**
+     * Discord prefix for text commands
+     * Used to make a global config for text commands (e.g.: !test)
+     */
+    commandPrefix?: string;
+    /**
+     * Bot owner ids
+     */
+    owners?: string[];
+    /**
+     * Discord status message shown in the members list and bot profile
+     */
+    statusMessage?: string;
+    /**
+     * Discord activity type going along-side statusMessage
+     * Can be standalone
+     */
+    activityType?: "PLAYING" | "WATCHING" | "LISTENING";
   };
 }
 
 const config: DiscordBotConfig = {
   main: {
-    /**
-     * Discord application/bot ID used for identification
-     * Required for registering slash commands and API interactions
-     */
     id: env.DISCORD_BOT_ID,
-    /**
-     * Discord bot authentication token
-     * Used to authenticate and login the bot to Discord's gateway
-     */
     token: env.DISCORD_BOT_TOKEN,
   },
 };
